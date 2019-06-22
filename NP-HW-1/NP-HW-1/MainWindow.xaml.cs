e263df71fb4cd1409cbbecb3d62a996b610f79a0
Azat <46015824+serikovskii@users.noreply.github.com>
2019-06-22 16:44:11 +0600

    done

---
+++ NP-HW-1/NP-HW-1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NP_HW_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SearchButton(object sender, RoutedEventArgs e)
+        {
+            var hostDomen = Dns.GetHostEntry(ipOrDomen.Text);
+            result.Text = hostDomen.HostName;
+            foreach (IPAddress ip in hostDomen.AddressList)
+            {
+                result.Text += $" \n{ip.ToString()}";
+            }
+        }
+    }
+}
